e65f686187415f1198f8b8a87c2aeeb3598f4057
Peyton Jones <peytonj.dev@gmail.com>
2020-08-20 21:05:59 -0600

    iteration 1 complete

--- src/book.js
+++ src/book.js
@@ -1,12 +1,50 @@
-function createTitle() {
-  
+function createTitle(bookTitle) {
+  return("The " + bookTitle)
 }
+
+function buildMainCharacter(name, age, pronouns) {
+  var characterObj = {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  }
+
+  return characterObj
+}
+
+function saveReview(newUniqueReview, reviewList) {
+  if(reviewList.indexOf(newUniqueReview)=== -1){
+    reviewList.push(newUniqueReview)
+  }
+}
+
+function calculatePageCount(bookTitle) {
+  var titleLength = bookTitle.length
+  return titleLength * 20
+}
+
+function writeBook(bookTitle, bookCharacter, genre) {
+  var bookObj = {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: calculatePageCount(bookTitle),
+    genre: genre
+  }
+  return bookObj
+}
+
+function editBook(book) {
+ book.pageCount = ((book.pageCount / 4) * 3)
+}
+
 
 module.exports = {
   createTitle: createTitle,
-  // buildMainCharacter: buildMainCharacter,
-  // saveReview: saveReview,
-  // calculatePageCount: calculatePageCount,
-  // writeBook: writeBook,
-  // editBook: editBook
+
+
+  buildMainCharacter: buildMainCharacter,
+  saveReview: saveReview,
+  calculatePageCount: calculatePageCount,
+  writeBook: writeBook,
+  editBook: editBook
 }
