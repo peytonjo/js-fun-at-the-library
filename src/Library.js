e47bf6a0f01e387a38dbb8c1ec94e1cfc802b81e
Peyton Jones <peytonj.dev@gmail.com>
2020-08-23 10:42:16 -0600

    shelf and libary complete

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,41 @@
 
+function createLibrary(buildingName){
+ var library = {
+   name: buildingName,
+   shelves:{
+     fantasy: [],
+     fiction: [],
+     nonFiction: []
+   }
+ }
+ return library
+}
+
+function addBook(place, book){
+  place.shelves[book.genre].push(book)
+}
+
+function checkoutBook(place, bookTitle){
+  var answer = "Sorry, there are currently no copies of The Fifth Season available at the Denver Public Library";
+  for (var genre in place.shelves) {
+    for (var i = 0; i < place.shelves[genre].length; i++) {
+      if (place.shelves[genre][i].title === bookTitle) {
+        place.shelves[genre].splice(i, 1)
+        answer = `You have now checked out ${bookTitle} from the Denver Public Library`
+      }
+    }
+  }
+  return answer;
+}
+
+
+
+
+
+
 
 module.exports = {
-  // createLibrary: createLibrary,
-  // addBook: addBook,
-  // checkoutBook: checkoutBook
+  createLibrary: createLibrary,
+  addBook: addBook,
+  checkoutBook: checkoutBook
 };
--- src/shelf.js
+++ src/shelf.js
@@ -1,8 +1,42 @@
 
+function shelfBook(book, shelf){
+  if(shelf.length < 3 ){
+    shelf.unshift(book)
+  }
+}
+
+function unshelfBook(name, sciFiShelf){
+  for (var i = 0; i < sciFiShelf.length; i++) {
+    if(sciFiShelf[i].title === name){
+      sciFiShelf.splice(i, 1)
+    }
+  }
+}
+
+function listTitles(shelf){
+  return `${shelf[0].title}, ${shelf[1].title}, ${shelf[2].title}`
+}
+
+
+
+function searchShelf(shelf, name){
+  var isThere = false
+  for (var i = 0; i < shelf.length; i++) {
+    if (name === shelf[i].title){
+    isThere = true;
+    }
+  }
+  return isThere
+}
+
+
+
+
+
 
 module.exports = {
-  // shelfBook: shelfBook,
-  // unshelfBook: unshelfBook,
-  // listTitles: listTitles,
-  // searchShelf: searchShelf
+   shelfBook: shelfBook,
+   unshelfBook: unshelfBook,
+  listTitles: listTitles,
+  searchShelf: searchShelf
 };
